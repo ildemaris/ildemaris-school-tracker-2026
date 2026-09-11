4c4b0962efb2a26af435263bf418886f17b528df
ildemaris <ildemaris.solano@escola.pr.gov.br>
2026-09-11 09:27:34 -0300

    Update emergencia.js

--- emergencia.js
+++ emergencia.js
@@ -3,9 +3,9 @@ import { getDatabase, ref, onValue, update } from "https://www.gstatic.com/fireb
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js&quot;;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCHxMR6NRrqiNSypsmxywtfu5x9H6ZlvOA",
-    authDomain: "school-tracker-2026.firebaseapp.com",
-    databaseURL: "https://school-tracker-2026-default-rtdb.firebaseio.com&quot;,
+    apiKey: "Minha API",
+    authDomain: "Meu Domínio",
+    databaseURL: "Minha URL";,
     projectId: "school-tracker-2026"
 };
 
@@ -28,7 +28,7 @@ function iniciarCronometro() {
     }, 1000);
 }
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, (user) => { 
     if (user) {
         console.log("🔒 Autenticação confirmada na Emergência. UID:", user.uid);
 
